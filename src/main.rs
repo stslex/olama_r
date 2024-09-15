e26abb82065be3d8f87171bd6041fa7ccd355ad1
stslex <ilya977.077@gmail.com>
2024-09-15 18:04:03 +0300

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use ollama_rs::{generation::completion::request::GenerationRequest, Ollama};
+
+#[tokio::main]
+async fn main() {
+    let ollama = Ollama::default();
+
+    // For custom values:
+    // let ollama = Ollama::new("http://localhost".to_string(), 11434);
+    let model = "llama3.1:latest".to_string();
+    let prompt = "test initial prompt".to_string();
+    let res = ollama.generate(GenerationRequest::new(model, prompt)).await;
+    println!("result :{:?}", res.unwrap().response);
 }
